dd70dfa866aa69444d2a0de7d9d6c35a1c22cb21
Elena Menzl <Elena.Menzl@gmx.de>
2015-07-10 10:14:04 +0200

    constructor

--- src/main/java/de/gymnew/sudoku/model/Sudoku.java
+++ src/main/java/de/gymnew/sudoku/model/Sudoku.java
@@ -4,6 +4,10 @@ public class Sudoku {
 
 	private Field[][] fields;
 
+	public Sudoku() {
+		fields = new Field[9][9];
+	}
+
 	public Field getField(int column, int row) {
 		return fields[column][row];
 	}
