433e492e3f31a177ea8006340b32e974da538212
Tobias Bodensteiner <bodensteiner.tobias@gmail.com>
2015-07-10 10:15:52 +0200

    added getters and methods

--- src/main/java/de/gymnew/sudoku/model/Cluster.java
+++ src/main/java/de/gymnew/sudoku/model/Cluster.java
@@ -5,4 +5,30 @@ import java.util.Set;
 public abstract class Cluster {
 
 	private Set<Field> fields;
+
+	public Cluster(Set<Field> fields) {
+		this.fields = fields;
+	}
+
+	public Set<Field> getFields() {
+		return fields;
+	}
+
+	public boolean hasValue(byte value) {
+		for (Field f : fields) {
+			if (f.getValue() == value)
+				return true;
+		}
+		return false;
+	}
+
+	public byte countNotes(byte note) {
+		byte count = 0;
+		for (Field f : fields) {
+			if (f.getNotes() == note)
+				count++;
+		}
+		return count;
+	}
+
 }
