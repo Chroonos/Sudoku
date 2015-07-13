11fa38c3588a1e1fd9bf34504ce23395305860da
Tobias Bodensteiner <bodensteiner.tobias@gmail.com>
2015-07-13 14:15:48 +0200

    constructors for row and column

--- src/main/java/de/gymnew/sudoku/model/Column.java
+++ src/main/java/de/gymnew/sudoku/model/Column.java
@@ -2,4 +2,10 @@ package de.gymnew.sudoku.model;
 
 public class Column extends Cluster {
 
+	public Column(int column, Sudoku sudoku){
+		super();
+		for(int i = 0; i<9; i++){
+			fields.add(sudoku.getField(column, i));
+		}
+	}
 }
