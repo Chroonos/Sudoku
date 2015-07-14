3fcbd1687f38b75f58e1336e37e55e6134933961
Sven Gebauer <chronos@procnull.de>
2015-07-14 15:49:38 +0200

    Add Clusters as attributes to Sudoku

--- src/main/java/de/gymnew/sudoku/model/Sudoku.java
+++ src/main/java/de/gymnew/sudoku/model/Sudoku.java
@@ -5,6 +5,9 @@ import java.io.File;
 public class Sudoku {
 
 	private Field[][] fields;
+	private Row[] rows;
+	private Column[] cols;
+	private Block[][] blocks;
 
 	public Sudoku() {
 		fields = new Field[9][9];
@@ -14,6 +17,7 @@ public class Sudoku {
 				fields[i][j] = new Field();
 			}
 		}
+		// TODO Create Rows, Columns and Blocks
 	}
 
 	public Field getField(int column, int row) {
