565d659f5d21ae7b6973dd3b3de9b4f8bddcb4dd
Tobias Bodensteiner <bodensteiner.tobias@gmail.com>
2015-07-17 10:07:58 +0200

    created class Standard

---
+++ src/main/java/de/gymnew/sudoku/algorithm/Standard.java
@@ -0,0 +1,14 @@
+package de.gymnew.sudoku.algorithm;
+
+import de.gymnew.sudoku.model.Algorithm;
+import de.gymnew.sudoku.model.Sudoku;
+
+public class Standard implements Algorithm {
+
+	@Override
+	public Sudoku solve(Sudoku sudoku) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
