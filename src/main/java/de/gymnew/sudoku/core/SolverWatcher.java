b221792a478b23c76b1272b0c2c16782b4868863
Tobias Grömer <tobias.groemer@gmail.com>
2015-07-28 09:16:42 +0200

    correct method spelling

--- src/main/java/de/gymnew/sudoku/core/SolverWatcher.java
+++ src/main/java/de/gymnew/sudoku/core/SolverWatcher.java
@@ -3,11 +3,11 @@ package de.gymnew.sudoku.core;
 import de.gymnew.sudoku.model.Sudoku;
 
 public interface SolverWatcher {
-	
+
 	public void onUpdate(Solver solver, Sudoku sudoku);
-	
-	public void onFinised(Solver solver);
-	
+
+	public void onFinished(Solver solver);
+
 	public void onInterrupted(Solver solver);
-	
+
 }
