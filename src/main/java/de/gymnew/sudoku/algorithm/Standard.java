fcf2aa0f0dc13d01f0492dc8af4cae36395fe2c7
Tobias Grömer <tobias.groemer@gmail.com>
2015-07-28 09:21:18 +0200

    try to make notes thread safe

--- src/main/java/de/gymnew/sudoku/algorithm/Standard.java
+++ src/main/java/de/gymnew/sudoku/algorithm/Standard.java
@@ -9,12 +9,12 @@ import de.gymnew.sudoku.model.Row;
 import de.gymnew.sudoku.model.Sudoku;
 
 public class Standard implements Algorithm {
-	
+
 	private Sudoku sudoku;
 	private boolean rebuildNotes;
 	private boolean madeChanges;
 	private Solver solver;
-	
+
 	public Standard(Sudoku sudoku, boolean rebuildNotes) {
 		this.sudoku = sudoku;
 		this.rebuildNotes = rebuildNotes;
@@ -23,46 +23,52 @@ public class Standard implements Algorithm {
 	public void setSolver(Solver solver) {
 		this.solver = solver;
 	}
-	
+
 	public Sudoku getSudoku() {
 		return sudoku;
 	}
-	
+
 	@Override
-	public Sudoku solve() throws InterruptedException{
-		if(rebuildNotes) createNotes();
-		
+	public Sudoku solve() throws InterruptedException {
+		if (rebuildNotes)
+			createNotes();
+
 		do {
 			madeChanges = false;
-			if(isSolved()) return sudoku;
-			if(Thread.interrupted()) throw new InterruptedException();
+			if (isSolved())
+				return sudoku;
+			if (Thread.interrupted())
+				throw new InterruptedException();
 			solver.getWatcher().onUpdate(solver, sudoku);
-			
+
 			singleNoteToValue();
-			if(madeChanges) continue;
-			
+			if (madeChanges)
+				continue;
+
 			singleNoteInClusterToValue();
-			if(madeChanges) continue;
-			
-		} while(madeChanges);
-		
+			if (madeChanges)
+				continue;
+
+		} while (madeChanges);
+
 		tryRandom();
-		if(madeChanges) return sudoku;
-		else return null;
+		if (madeChanges)
+			return sudoku;
+		else
+			return null;
 	}
-	
-	/*==================================================*/
+
+	/* ================================================== */
 	// Non-Deterministic algorithm parts
-	/*==================================================*/
-	
+	/* ================================================== */
+
 	private void tryRandom() throws InterruptedException {
 		Sudoku clone = sudoku.clone();
 		for (int k = 2; k < 10; k++) {
 			for (int i = 0; i < 9; i++) {
 				for (int j = 0; j < 9; j++) {
-					if (sudoku.getField(i, j).getNotes().size() == k) {
-						Byte[] notes = sudoku.getField(i, j).getNotes()
-								.toArray(new Byte[k]);
+					if (sudoku.getField(i, j).countNotes() == k) {
+						Byte[] notes = sudoku.getField(i, j).getNotes().toArray(new Byte[k]);
 						int l = 0;
 						while (l < k) {
 							byte b = notes[l];
@@ -80,64 +86,64 @@ public class Standard implements Algorithm {
 			}
 		}
 	}
-	
-	/*==================================================*/
+
+	/* ================================================== */
 	// Deterministic algorithm parts
-	/*==================================================*/
-	
+	/* ================================================== */
+
 	private void createNotes() {
 		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9;j++) {
+			for (int j = 0; j < 9; j++) {
 				Field field = sudoku.getField(i, j);
 				field.clearNotes();
-				
+
 				for (byte k = 1; k < 10; k++) {
 					if (field.getValue() == 0 && field.canHaveValue(k)) {
-							field.addNote(k);
+						field.addNote(k);
 					}
 				}
 			}
 		}
 	}
-	
+
 	private void singleNoteToValue() {
-		for (int i = 0; i < 9; i++){
+		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				Field field = sudoku.getField(i, j);
-				if (field.getNotes().size() == 1) {
+				if (field.countNotes() == 1) {
 					byte b = field.getNotes().toArray(new Byte[1])[0];
 					setValue(field, b);
 				}
 			}
 		}
 	}
-	
-	private void singleNoteInClusterToValue(){
-		for(Column c : sudoku.getColumns()){
-			for(int i = 1; i < 10; i++){
-				if(c.countNotes((byte) i) == 1){
-					for(Field f : c.getFieldsWithNote((byte) i)){
+
+	private void singleNoteInClusterToValue() {
+		for (Column c : sudoku.getColumns()) {
+			for (int i = 1; i < 10; i++) {
+				if (c.countNotes((byte) i) == 1) {
+					for (Field f : c.getFieldsWithNote((byte) i)) {
 						setValue(f, (byte) i);
 					}
 				}
 			}
 		}
-		
-		for(Row r : sudoku.getRows()){
-			for(int i = 1; i < 10; i++){
-				if(r.countNotes((byte) i) == 1){
-					for(Field f : r.getFieldsWithNote((byte) i)){
+
+		for (Row r : sudoku.getRows()) {
+			for (int i = 1; i < 10; i++) {
+				if (r.countNotes((byte) i) == 1) {
+					for (Field f : r.getFieldsWithNote((byte) i)) {
 						setValue(f, (byte) i);
 					}
 				}
 			}
 		}
-		
-		for(Block[] blocks : sudoku.getBlocks()){
-			for(Block b : blocks){
-				for(int i = 1; i < 10; i++){
-					if(b.countNotes((byte) i) == 1){
-						for(Field f : b.getFieldsWithNote((byte) i)){
+
+		for (Block[] blocks : sudoku.getBlocks()) {
+			for (Block b : blocks) {
+				for (int i = 1; i < 10; i++) {
+					if (b.countNotes((byte) i) == 1) {
+						for (Field f : b.getFieldsWithNote((byte) i)) {
 							setValue(f, (byte) i);
 						}
 					}
@@ -145,26 +151,26 @@ public class Standard implements Algorithm {
 			}
 		}
 	}
-	
-	/*==================================================*/
+
+	/* ================================================== */
 	// Auxiliary methods
-	/*==================================================*/
-	
+	/* ================================================== */
+
 	private void setValue(Field field, byte value) {
 		field.clearNotes();
 		field.setValue(value);
-		
+
 		field.getRow().removeNotes(value);
 		field.getColumn().removeNotes(value);
 		field.getBlock().removeNotes(value);
-		
+
 		madeChanges = true;
 	}
-	
+
 	private boolean isSolved() {
-		for(int i = 0; i < 9; i++) {
-			for(int j = 0; j < 9; j++) {
-				if(sudoku.getField(i, j).getValue() == 0)
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (sudoku.getField(i, j).getValue() == 0)
 					return false;
 			}
 		}
--- src/main/java/de/gymnew/sudoku/core/Solver.java
+++ src/main/java/de/gymnew/sudoku/core/Solver.java
@@ -4,23 +4,24 @@ import de.gymnew.sudoku.model.Algorithm;
 import de.gymnew.sudoku.model.Sudoku;
 
 public class Solver extends Thread {
-	
+
 	private Algorithm algorithm;
 	private Sudoku result;
 	private SolverWatcher watcher;
-	
+
 	public Solver(Algorithm algorithm, SolverWatcher watcher) {
 		this.algorithm = algorithm;
 		result = null;
 		this.watcher = watcher;
 		algorithm.setSolver(this);
 	}
-	
+
 	@Override
 	public void run() {
 		try {
 			result = algorithm.solve();
-		} catch(InterruptedException e) {
+			watcher.onFinished(this);
+		} catch (InterruptedException e) {
 			watcher.onInterrupted(this);
 		}
 	}
@@ -28,11 +29,11 @@ public class Solver extends Thread {
 	public Sudoku getResult() {
 		return result;
 	}
-	
+
 	public SolverWatcher getWatcher() {
 		return watcher;
 	}
-	
+
 	public Algorithm getAlgorithm() {
 		return algorithm;
 	}
